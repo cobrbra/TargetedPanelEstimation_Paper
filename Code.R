ac9b56e9289794488b10c52efbe3c9f7b15bdc14
Jacob Bradley <cobrbradley@gmail.com>
2020-12-30 16:11:43 +0000

    Old repository docs

---
+++ Code.R
@@ -0,0 +1,136 @@
+## R Packages
+#install.packages("tidyverse")
+library(tidyverse)
+#install.packages("cowplot")
+library(cowplot)
+#install.packages("devtools")
+library(devtools)
+
+## ICBioMark Package
+#devtools::install_github("cobrbra/ICBioMark", upgrade = "ask")
+library(ICBioMark)
+
+## Figures path
+fig_path <- "InProgress/TargetedPanelDesign/figures/"
+
+
+
+### Main workflow
+nsclc_tables <- get_mutation_tables(maf = nsclc_maf, include_synonymous = FALSE)
+
+
+
+### Figure 1
+
+
+
+## Subfigure 1
+fig1p1 <- nsclc_survival %>% 
+  add_count(SEX, name = "n_sex") %>% 
+  mutate(Sex = SEX, Age = as.numeric(AGE)) %>% 
+  filter(Sex %in% c("Male", "Female")) %>% 
+  ggplot(aes(x = Sex, y = Age)) + geom_violin(fill = "black", alpha = 0.5, colour = "black") + 
+  geom_text(aes(x = Sex, y = 67, label = paste("n = ", n_sex))) + theme_minimal() + 
+  theme(axis.title.x=element_blank()) 
+
+stage_groups <- c("I", "I", "I", "II", "II", "II", "III", "III", "III", "IV")
+names(stage_groups) <- c("I", "IA", "IB", "II", "IIA", "IIB", "III", "IIIA", "IIIB", "IV")
+
+smoking_groups <- c("Lifelong Non-Smoker", "Current Reformed \n Smoker (>15 Years)",
+                    "Current Reformed \n Smoker (<= 15 Years)", "Current Reformed \n Smoker (Duration UnSpecified)",
+                    "Current Smoker")
+names(smoking_groups) <- c("Lifelong Non-Smoker", "Current Reformed Smoker For > 15 Years",
+                           "Current Reformed Smoker For < Or = 15 Years", "Current Reformed Smoker, Duration Not Specified",
+                           "Current Smoker")
+plot(fig1p1)
+
+fig1p2 <- nsclc_survival %>% 
+  group_by(SMOKING_HISTORY, STAGE) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(Stage = stage_groups[STAGE]) %>% 
+  filter(!is.na(Stage)) %>% 
+  mutate(Stage = factor(Stage, levels = rev(c("I","II","III","IV")))) %>% 
+  mutate(Smoking_History = smoking_groups[SMOKING_HISTORY])  %>%  
+  filter(!is.na(Smoking_History)) %>% 
+  select(Stage, Smoking_History, n) %>% 
+  group_by(Stage, Smoking_History) %>% 
+  mutate(n = sum(n)) %>% 
+  distinct() %>% 
+  ggplot(aes(x = Smoking_History, y = n, fill = Stage, order = Stage)) + 
+  geom_col(position = position_stack(reverse = TRUE), alpha = 0.8, colour = "black") + scale_fill_brewer(palette = "Greys") +
+  theme_minimal() + theme(axis.text.x = element_text(angle = 45, vjust = 0.7)) +
+  labs(y = "Frequency") + theme(axis.title.x=element_blank())
+
+plot(fig1p2)
+fig1 <- plot_grid(fig1p1, fig1p2, labels = "AUTO", align = "h")
+print(fig1)
+ggsave(paste0(fig_path, "fig1.png"), fig1, width = 12, height = 5)
+
+
+
+### Figure 2
+
+
+
+tmb_tib_train <- inner_join(get_biomarker_tables(maf = nsclc_maf, biomarker = "TMB")$train, get_biomarker_tables(maf = nsclc_maf, biomarker = "TIB")$train)
+
+## Subfigure 1
+fig2p1 <- tmb_tib_train %>%
+  gather(key = "Biomarker", value = "Count", -Tumor_Sample_Barcode) %>%
+  mutate(Biomarker = factor(Biomarker, levels = c("TMB", "TIB"))) %>%
+  ggplot(aes(x = Biomarker, y = Count)) +
+  geom_violin(fill = "black", alpha = 0.5, colour = "black") +
+  scale_y_log10() +
+  theme_minimal() +
+  labs(y = "Mutation Count") + theme(axis.title.x = element_blank())
+
+print(fig2p1)
+
+## Subfigure 2
+variant_names <- c("Missense", "Nonsense", "Splice site", "Translation start",
+                   "Nonstop", "In-frame indel", "In-frame indel", "Frame-shift indel",
+                   "Frame-shift indel")
+names(variant_names) <- names(get_mutation_dictionary(include_synonymous = FALSE))
+
+fig2p2 <- nsclc_maf %>%
+  filter(Tumor_Sample_Barcode %in% nsclc_tables$train$sample_list) %>%
+  filter(Variant_Classification %in% names(variant_names)) %>%
+  mutate(Variant_Classification = variant_names[Variant_Classification]) %>%
+  group_by(Variant_Classification) %>%
+  count() %>%
+  ungroup() %>%
+  mutate(n = n/sum(n)) %>%
+  ggplot(aes(x = reorder(Variant_Classification, - n), y = n)) + 
+  geom_col(fill = "black", alpha = 0.5, colour = "black") + labs(y = "Proportion of Nonsynonymous Mutations") +
+  theme_minimal() + theme(axis.text.x = element_text(angle = 45, vjust = 0.8)) +
+  theme(axis.title.x=element_blank())
+
+print(fig2p2)
+
+fig2 <- plot_grid(fig2p1, fig2p2, labels = "AUTO", align = "h")
+print(fig2)
+ggsave(paste0(fig_path, "fig2.png"), fig2, width = 12, height = 5)
+
+
+
+
+### Section 3 Intro Stats
+n_train_samples <- length(nsclc_tables$train$sample_list)
+n_val_samples <- length(nsclc_tables$va$sample_list)
+n_test_samples <- length(nsclc_tables$test$sample_list)
+print(paste("Training Samples:", n_train_samples))
+print(paste("Validation Samples:", n_val_samples))
+print(paste("Test Samples:", n_test_samples))
+
+n_genes <- length(unique(nsclc_maf$Hugo_Symbol))
+print(paste("Genes Analysed:", n_genes))
+
+tmb_mean <- mean(tmb_tib_train$TMB)
+tib_mean <- mean(tmb_tib_train$TIB)
+print(paste("TMB Mean:", tmb_mean))
+print(paste("TIB Mean:", tib_mean))
+
+
+
+### Figure 3
---
+++ data/docs.R
@@ -0,0 +1,105 @@
+#' Foundation Panel Gene List
+#' 
+#' Gene list for the FoundationOne targeted gene panel.
+#' 
+#' @format A dataframe with one column:
+#'   \describe{
+#'     \item{Hugo_Symbol}{The genes constituting the panel}
+#'   }
+#' @source \url{https://assets.ctfassets.net/w98cd481qyp0/YqqKHaqQmFeqc5ueQk48w/0a34fcdaa3a71dbe460cdcb01cebe8ad/F1CDx_Technical_Specifications_072020.pdf}
+"foundation_genes"
+
+#' TruSight Tumor 170 Panel Gene List
+#' 
+#' Gene list for the TST-170 targeted gene panel.
+#' 
+#' @format A dataframe with one column:
+#'   \describe{
+#'     \item{Hugo_Symbol}{The genes constituting the panel}
+#'   }
+#' @source \url{https://emea.illumina.com/content/dam/illumina-marketing/documents/products/datasheets/trusight-tumor-170-data-sheet-1170-2016-017.pdf}
+"tst_170_genes"
+
+#' TruSight Oncology 500 Panel Gene List
+#' 
+#' Gene list for the TSO-500 targeted gene panel.
+#' 
+#' @format A dataframe with one column:
+#'   \describe{
+#'     \item{Hugo_Symbol}{The genes constituting the panel}
+#'   }
+#' @source \url{http://albiogen.ru/upload/iblock/8e6/8e66914c1b78f53509fe9186d80f8d75.pdf}
+"tso_500_genes"
+
+#' Memorial-Sloan Kettering IMPACT Panel Gene List
+#' 
+#' Gene list for the MSK-IMPACT targeted gene panel.
+#' 
+#' @format A dataframe with one column:
+#'   \describe{
+#'     \item{Hugo_Symbol}{The genes constituting the panel}
+#'   }
+#' @source \url{https://www.oncokb.org/cancerGenes}
+"tso_500_genes"
+
+
+#' Foundation Panel BED
+#' 
+#' BED file (exon locations) for the FoundationOne targeted gene panel.
+#' 
+#' @format A dataframe with four columns:
+#'   \describe{
+#'     \item{Chromosome}{The chromosomal location of the exon.}
+#'     \item{Start}{The start nucleotide base of the exon}
+#'     \item{End}{The end nucleotide base of the exon}
+#'     \item{name}{Exon name}
+#'   }
+#' @source Produced from foundation_genes dataframe using the UCSC table 
+#' browser at \url{https://genome.ucsc.edu/cgi-bin/hgTables}
+"foundation_bed"
+
+#' TruSight Tumor 170 Panel BED
+#' 
+#' BED file (exon locations) for the TST-170 targeted gene panel.
+#' 
+#' @format A dataframe with four columns:
+#'   \describe{
+#'     \item{Chromosome}{The chromosomal location of the exon.}
+#'     \item{Start}{The start nucleotide base of the exon}
+#'     \item{End}{The end nucleotide base of the exon}
+#'     \item{name}{Exon name}
+#'   }
+#' @source Produced from tso_170_genes dataframe using the UCSC table 
+#' browser at \url{https://genome.ucsc.edu/cgi-bin/hgTables}
+"tst_170_bed"
+
+#' TruSight Oncology Panel BED
+#' 
+#' BED file (exon locations) for the TSO-500 targeted gene panel.
+#' 
+#' @format A dataframe with four columns:
+#'   \describe{
+#'     \item{Chromosome}{The chromosomal location of the exon.}
+#'     \item{Start}{The start nucleotide base of the exon}
+#'     \item{End}{The end nucleotide base of the exon}
+#'     \item{name}{Exon name}
+#'   }
+#' @source Produced from tso_500_genes dataframe using the UCSC table 
+#' browser at \url{https://genome.ucsc.edu/cgi-bin/hgTables}
+"tso_500_bed"
+
+#' MSK-IMPACT Panel BED
+#' 
+#' BED file (exon locations) for the MSK-IMPACT targeted gene panel.
+#' 
+#' @format A dataframe with four columns:
+#'   \describe{
+#'     \item{Chromosome}{The chromosomal location of the exon.}
+#'     \item{Start}{The start nucleotide base of the exon}
+#'     \item{End}{The end nucleotide base of the exon}
+#'     \item{name}{Exon name}
+#'   }
+#' @source Produced from msk_impact_genes dataframe using the UCSC table 
+#' browser at \url{https://genome.ucsc.edu/cgi-bin/hgTables}
+"msk_impact_bed"
+
