d705362bb9bbeddaeaaa57cd6adee9810e57ed9c
Jacob Bradley <cobrbradley@gmail.com>
2021-01-11 23:42:10 +0000

    upping scheduled run-time (d'oh)

--- analysis.sh
+++ analysis.sh
@@ -2,7 +2,7 @@
 # Grid Engine options (lines prefixed with #$)
 #$ -N analysis              
 #$ -cwd                  
-#$ -l h_rt=00:02:00 
+#$ -l h_rt=02:00:00 
 #$ -l h_vmem=100G
 
 # Initialise the environment modules
