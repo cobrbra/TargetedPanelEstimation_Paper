0cabcbaba5f2ba9a1bbb1dce7e2b255936c6e2cb
Jacob Bradley <cobrbradley@gmail.com>
2021-01-11 18:47:43 +0000

    updating analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -8,7 +8,6 @@
 # Initialise the environment modules
 . /etc/profile.d/modules.sh
 
-## RUN THIS FIRST
-#export R_LIBS_USER=/exports/eddie/scratch/s1505825/R/library; . /etc/profile.d/modules.sh; module load igmm/apps/R/3.6.3; module load igmm/apps/BEDTools/2.27.1
+export R_LIBS_USER=/exports/eddie/scratch/s1505825/R/library; . /etc/profile.d/modules.sh; module load igmm/apps/R/3.6.3; module load igmm/apps/BEDTools/2.27.1
 
-R CMD BATCH Code.R
+Rscript Code.R
