2fec507cf2ae57e76bf3784feccf54f459e46312
Jacob Bradley <cobrbradley@gmail.com>
2021-03-01 20:19:53 +0000

    forgot to change Rscript location in analysis.sh for CodeOcean restructure

--- analysis.sh
+++ analysis.sh
@@ -10,7 +10,7 @@
 
 export R_LIBS_USER=/exports/eddie/scratch/s1505825/R/library; . /etc/profile.d/modules.sh; module load igmm/apps/R/3.6.3; module load igmm/apps/BEDTools/2.27.1
 
-Rscript Code.R
+Rscript code/Code.R
 
 # Notes to self on pushing from Eddie:
 #--------------------------------------
