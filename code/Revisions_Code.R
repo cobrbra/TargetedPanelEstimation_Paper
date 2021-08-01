be3d369dc7d6f2e0223433c1839048c141dbf2e7
Jacob Bradley <cobrbradley@gmail.com>
2021-08-01 15:56:21 +0100

    forest plots for MSK-IMPACT validation

--- code/Revisions_Code.R
+++ code/Revisions_Code.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(devtools)
 library(latex2exp)
 library(cowplot)
+library(survival)
+library(survminer)
 load_all("../../ICBioMark/")
 
 ### Melanoma analysis
@@ -24,6 +26,9 @@ skcm_pred_first_tmb <- pred_first_fit(gen_model = skcm_gen_model,
                                       gene_lengths = ensembl_gene_lengths, 
                                       training_matrix = skcm_tables$train$matrix,
                                       marker_mut_types = c("NS"))
+
+write_rds(x = skcm_pred_first_tmb, "skcm_pred_first_tmb")
+write_rds(x = skcm_gen_model, "skcm_gen_model")
 
 skcm_pred_refit_tmb <- pred_refit_range(pred_first = skcm_pred_first_tmb, 
                                         gene_lengths = ensembl_gene_lengths,
@@ -241,3 +246,168 @@ joint_version_fig8 <- plot_grid(skcm_version_fig8 + ggtitle("Melanoma (SKCM)") +
                                 nrow = 3,
                                 labels = "AUTO")
 ggsave(joint_version_fig8, filename = "results/figures/joint_version_fig8.png", height = 9, width = 4)
+
+
+### MSK-IMPACT External Validation
+tmb_val_maf <- read_tsv("data/tmb_mskcc_2018/data_mutations_extended.txt") %>% 
+  select(Tumor_Sample_Barcode, Hugo_Symbol, Variant_Classification, Chromosome, Start_Position, End_Position)
+tmb_val_patient <- read_tsv("data/tmb_mskcc_2018/data_clinical_patient.txt", comment = "#")
+tmb_val_sample <- read_tsv("data/tmb_mskcc_2018/data_clinical_sample.txt", comment = "#")
+tmb_val_clinical <- inner_join(tmb_val_sample, tmb_val_patient, by = "PATIENT_ID") %>% 
+  select(SAMPLE_ID, CANCER_TYPE, TMB_SCORE, OS_MONTHS, OS_STATUS, DRUG_TYPE)
+msk_impact_genes <- read_tsv("data/msk_impact_genes.tsv")
+
+skcm_val_tables <- get_mutation_tables(tmb_val_maf, 
+                                   split = c(train = 0, val = 0, test = 320), 
+                                   sample_list = filter(tmb_val_clinical, CANCER_TYPE == "Melanoma")$SAMPLE_ID, 
+                                   gene_list = skcm_tables$train$gene_list,
+                                   acceptable_genes = ensembl_gene_lengths$Hugo_Symbol, 
+                                   for_biomarker = "TMB",
+                                   include_synonymous = FALSE)
+
+skcm_msk_predictions <- pred_refit_panel(pred_first = skcm_pred_first_tmb, 
+                 gene_lengths = ensembl_gene_lengths, 
+                 genes = msk_impact_genes$Hugo_Symbol,
+                 marker_mut_types = c("NS")) %>% 
+  get_predictions(new_data = skcm_val_tables$test) %>% 
+  {as.data.frame(.$predictions)}
+
+colnames(skcm_msk_predictions) <- c("Estimated_TMB")
+skcm_msk_predictions$Tumor_Sample_Barcode <- rownames(skcm_msk_predictions) 
+skcm_msk_predictions <- skcm_msk_predictions %>% 
+  inner_join(tmb_val_clinical, by = c("Tumor_Sample_Barcode"="SAMPLE_ID")) 
+
+
+
+skcm_surv_25 <- skcm_msk_predictions %>% 
+  {mutate(., surv = Surv(time = .$OS_MONTHS, event = (.$OS_STATUS == "1:DECEASED")))} %>% 
+  mutate("Estimated TMB" = if_else(Estimated_TMB >= quantile(Estimated_TMB, 0.25), "High", "Low"),
+         "Original TMB" = if_else(TMB_SCORE >= quantile(TMB_SCORE, 0.25), "High", "Low"))
+skcm_surv_50 <- skcm_msk_predictions %>% 
+  {mutate(., surv = Surv(time = .$OS_MONTHS, event = (.$OS_STATUS == "1:DECEASED")))} %>% 
+  mutate("Estimated TMB" = if_else(Estimated_TMB >= quantile(Estimated_TMB, 0.5), "High", "Low"),
+         "Original TMB" = if_else(TMB_SCORE >= quantile(TMB_SCORE, 0.5), "High", "Low"))
+skcm_surv_75 <- skcm_msk_predictions %>% 
+  {mutate(., surv = Surv(time = .$OS_MONTHS, event = (.$OS_STATUS == "1:DECEASED")))} %>% 
+  mutate("Estimated TMB" = if_else(Estimated_TMB >= quantile(Estimated_TMB, 0.75), "High", "Low"),
+         "Original TMB" = if_else(TMB_SCORE >= quantile(TMB_SCORE, 0.75), "High", "Low"))
+
+forest_25_est <- coxph(surv ~ `Estimated TMB`, data = skcm_surv_25) %>% 
+  ggforest(cpositions = c(0.0, 0.15,0.38), main = "25% Quantile")
+forest_25_true <- coxph(surv ~ `Original TMB`, data = skcm_surv_25) %>% 
+  ggforest(cpositions = c(0.0,0.15,0.38), main = "")
+forest_50_est <- coxph(surv ~ `Estimated TMB`, data = skcm_surv_50) %>% 
+  ggforest(cpositions = c(0.0, 0.15,0.38), main = "50% Quantile")
+forest_50_true <- coxph(surv ~ `Original TMB`, data = skcm_surv_50) %>% 
+  ggforest(cpositions = c(0.0,0.15,0.38), main = "")
+forest_75_est <- coxph(surv ~ `Estimated TMB`, data = skcm_surv_75) %>% 
+  ggforest(cpositions = c(0.0, 0.15,0.38), main = "75% Quantile")
+forest_75_true <- coxph(surv ~ `Original TMB`, data = skcm_surv_75) %>% 
+  ggforest(cpositions = c(0.0,0.15,0.38), main = "")
+
+skcm_forests <- plot_grid(forest_25_est, forest_25_true, forest_50_est, forest_50_true, forest_75_est, forest_75_true, ncol = 1)
+ggsave(skcm_forests, filename = "results/figures/skcm_forests.png", width = 7, height = 12)
+
+coadread_val_tables <- get_mutation_tables(tmb_val_maf, 
+                                       split = c(train = 0, val = 0, test = 110), 
+                                       sample_list = filter(tmb_val_clinical, CANCER_TYPE == "Colorectal Cancer")$SAMPLE_ID, 
+                                       gene_list = coadread_tables$train$gene_list,
+                                       acceptable_genes = ensembl_gene_lengths$Hugo_Symbol, 
+                                       for_biomarker = "TMB",
+                                       include_synonymous = FALSE)
+
+coadread_msk_predictions <- pred_refit_panel(pred_first = coadread_pred_first_tmb, 
+                                         gene_lengths = ensembl_gene_lengths, 
+                                         genes = msk_impact_genes$Hugo_Symbol,
+                                         marker_mut_types = c("NS")) %>% 
+  get_predictions(new_data = coadread_val_tables$test) %>% 
+  {as.data.frame(.$predictions)}
+
+colnames(coadread_msk_predictions) <- c("Estimated_TMB")
+coadread_msk_predictions$Tumor_Sample_Barcode <- rownames(coadread_msk_predictions) 
+coadread_msk_predictions <- coadread_msk_predictions %>% 
+  inner_join(tmb_val_clinical, by = c("Tumor_Sample_Barcode"="SAMPLE_ID")) 
+
+
+
+coadread_surv_25 <- coadread_msk_predictions %>% 
+  {mutate(., surv = Surv(time = .$OS_MONTHS, event = (.$OS_STATUS == "1:DECEASED")))} %>% 
+  mutate("Estimated TMB" = if_else(Estimated_TMB >= quantile(Estimated_TMB, 0.25), "High", "Low"),
+         "Original TMB" = if_else(TMB_SCORE >= quantile(TMB_SCORE, 0.25), "High", "Low"))
+coadread_surv_50 <- coadread_msk_predictions %>% 
+  {mutate(., surv = Surv(time = .$OS_MONTHS, event = (.$OS_STATUS == "1:DECEASED")))} %>% 
+  mutate("Estimated TMB" = if_else(Estimated_TMB >= quantile(Estimated_TMB, 0.5), "High", "Low"),
+         "Original TMB" = if_else(TMB_SCORE >= quantile(TMB_SCORE, 0.5), "High", "Low"))
+coadread_surv_75 <- coadread_msk_predictions %>% 
+  {mutate(., surv = Surv(time = .$OS_MONTHS, event = (.$OS_STATUS == "1:DECEASED")))} %>% 
+  mutate("Estimated TMB" = if_else(Estimated_TMB >= quantile(Estimated_TMB, 0.75), "High", "Low"),
+         "Original TMB" = if_else(TMB_SCORE >= quantile(TMB_SCORE, 0.75), "High", "Low"))
+
+forest_25_est <- coxph(surv ~ `Estimated TMB`, data = coadread_surv_25) %>% 
+  ggforest(cpositions = c(0.0, 0.15,0.38), main = "25% Quantile")
+forest_25_true <- coxph(surv ~ `Original TMB`, data = coadread_surv_25) %>% 
+  ggforest(cpositions = c(0.0,0.15,0.38), main = "")
+forest_50_est <- coxph(surv ~ `Estimated TMB`, data = coadread_surv_50) %>% 
+  ggforest(cpositions = c(0.0, 0.15,0.38), main = "50% Quantile")
+forest_50_true <- coxph(surv ~ `Original TMB`, data = coadread_surv_50) %>% 
+  ggforest(cpositions = c(0.0,0.15,0.38), main = "")
+forest_75_est <- coxph(surv ~ `Estimated TMB`, data = coadread_surv_75) %>% 
+  ggforest(cpositions = c(0.0, 0.15,0.38), main = "75% Quantile")
+forest_75_true <- coxph(surv ~ `Original TMB`, data = coadread_surv_75) %>% 
+  ggforest(cpositions = c(0.0,0.15,0.38), main = "")
+
+coadread_forests <- plot_grid(forest_25_est, forest_25_true, forest_50_est, forest_50_true, forest_75_est, forest_75_true, ncol = 1)
+ggsave(coadread_forests, filename = "results/figures/coadread_forests.png", width = 7, height = 12)
+
+blca_val_tables <- get_mutation_tables(tmb_val_maf, 
+                                           split = c(train = 0, val = 0, test = 110), 
+                                           sample_list = filter(tmb_val_clinical, CANCER_TYPE == "Colorectal Cancer")$SAMPLE_ID, 
+                                           gene_list = blca_tables$train$gene_list,
+                                           acceptable_genes = ensembl_gene_lengths$Hugo_Symbol, 
+                                           for_biomarker = "TMB",
+                                           include_synonymous = FALSE)
+
+blca_msk_predictions <- pred_refit_panel(pred_first = blca_pred_first_tmb, 
+                                             gene_lengths = ensembl_gene_lengths, 
+                                             genes = msk_impact_genes$Hugo_Symbol,
+                                             marker_mut_types = c("NS")) %>% 
+  get_predictions(new_data = blca_val_tables$test) %>% 
+  {as.data.frame(.$predictions)}
+
+colnames(blca_msk_predictions) <- c("Estimated_TMB")
+blca_msk_predictions$Tumor_Sample_Barcode <- rownames(blca_msk_predictions) 
+blca_msk_predictions <- blca_msk_predictions %>% 
+  inner_join(tmb_val_clinical, by = c("Tumor_Sample_Barcode"="SAMPLE_ID")) 
+
+
+
+blca_surv_25 <- blca_msk_predictions %>% 
+  {mutate(., surv = Surv(time = .$OS_MONTHS, event = (.$OS_STATUS == "1:DECEASED")))} %>% 
+  mutate("Estimated TMB" = if_else(Estimated_TMB >= quantile(Estimated_TMB, 0.25), "High", "Low"),
+         "Original TMB" = if_else(TMB_SCORE >= quantile(TMB_SCORE, 0.25), "High", "Low"))
+blca_surv_50 <- blca_msk_predictions %>% 
+  {mutate(., surv = Surv(time = .$OS_MONTHS, event = (.$OS_STATUS == "1:DECEASED")))} %>% 
+  mutate("Estimated TMB" = if_else(Estimated_TMB >= quantile(Estimated_TMB, 0.5), "High", "Low"),
+         "Original TMB" = if_else(TMB_SCORE >= quantile(TMB_SCORE, 0.5), "High", "Low"))
+blca_surv_75 <- blca_msk_predictions %>% 
+  {mutate(., surv = Surv(time = .$OS_MONTHS, event = (.$OS_STATUS == "1:DECEASED")))} %>% 
+  mutate("Estimated TMB" = if_else(Estimated_TMB >= quantile(Estimated_TMB, 0.75), "High", "Low"),
+         "Original TMB" = if_else(TMB_SCORE >= quantile(TMB_SCORE, 0.75), "High", "Low"))
+
+forest_25_est <- coxph(surv ~ `Estimated TMB`, data = blca_surv_25) %>% 
+  ggforest(cpositions = c(0.0, 0.15,0.38), main = "25% Quantile")
+forest_25_true <- coxph(surv ~ `Original TMB`, data = blca_surv_25) %>% 
+  ggforest(cpositions = c(0.0,0.15,0.38), main = "")
+forest_50_est <- coxph(surv ~ `Estimated TMB`, data = blca_surv_50) %>% 
+  ggforest(cpositions = c(0.0, 0.15,0.38), main = "50% Quantile")
+forest_50_true <- coxph(surv ~ `Original TMB`, data = blca_surv_50) %>% 
+  ggforest(cpositions = c(0.0,0.15,0.38), main = "")
+forest_75_est <- coxph(surv ~ `Estimated TMB`, data = blca_surv_75) %>% 
+  ggforest(cpositions = c(0.0, 0.15,0.38), main = "75% Quantile")
+forest_75_true <- coxph(surv ~ `Original TMB`, data = blca_surv_75) %>% 
+  ggforest(cpositions = c(0.0,0.15,0.38), main = "")
+
+blca_forests <- plot_grid(forest_25_est, forest_25_true, forest_50_est, forest_50_true, forest_75_est, forest_75_true, ncol = 1)
+ggsave(blca_forests, filename = "results/figures/blca_forests.png", width = 7, height = 12)
+
+
